93a3431c1d2115ced15b96886e138d86df5c655d
Oguz Alp Saglam <42783655+oguzalp7@users.noreply.github.com>
2021-11-21 05:29:18 +0300

    Add files via upload

--- Linear_Regression/main.py
+++ Linear_Regression/main.py
@@ -7,14 +7,14 @@ import LinearRegression_
 def main():
     df = pd.read_csv("linear_regression_dataset.csv", sep=";")
     lr = LinearRegression_.LinearRegression_(df)
-    # lr.visualize_data("deneyim", "maas")
+    lr.visualize_data("experience", "salary")
 
-    # print("Prediction on the second index: " ,lr.single_prediction("deneyim", "maas", 2))
+    print("Prediction on the second index: ", lr.single_prediction("experience", "salary", 2))
 
-    # temp = [30, 40, 60]
-    # print(lr.multiple_predictions("deneyim", "maas", temp))
+    indexes = [30, 40, 60]
+    print("Predictions at 30th, 40th, 60th indexes", lr.multiple_predictions("experience", "salary", indexes))
 
-    # lr.visualize_results("deneyim", "maas", 50)
+    lr.visualize_results("experience", "salary", 50)
         
         
 if __name__ == '__main__':
